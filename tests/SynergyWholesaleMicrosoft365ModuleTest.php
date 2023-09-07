4187897193b57c8ed257f4a72c72b7adf104749d
Manh Huy Vo <manhhuyvo@gmail.com>
2023-09-07 11:52:11 +1000

    feat: automation tests

---
+++ tests/SynergyWholesaleMicrosoft365ModuleTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SynergyWholesale\WHMCS\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Synergy Wholesale Microsoft 365 Module Test
+ *
+ * PHPUnit test that asserts the fundamental requirements of a WHMCS
+ * server module.
+ *
+ * Custom module tests are added in addition.
+ *
+ * @copyright Copyright (c) Synergy Wholesale Pty Ltd 2020
+ * @license https://github.com/synergywholesale/whmcs-microsoft365-module/LICENSE
+ */
+
+class SyenrgyWholesaleMicrosoft365ModuleTest extends TestCase
+{
+    public static function providerCoreFunctionNames()
+    {
+        return [
+            ['CreateAccount'],
+            ['SuspendAccount'],
+            ['UnsuspendAccount'],
+            ['TerminateAccount'],
+            ['ChangePackage'],
+            ['AdminCustomButtonArray'],
+            ['ConfigOptions'],
+            ['MetaData'],
+            ['ClientArea'],
+            ['sync'],
+        ];
+    }
+
+    /**
+     * Test Core Module Functions Exist
+     *
+     * This test confirms that the functions recommended by WHMCS (and more)
+     * are defined in this module.
+     *
+     * @param $method
+     *
+     * @dataProvider providerCoreFunctionNames
+     */
+    public function testCoreModuleFunctionsExist($method)
+    {
+        $this->assertTrue(function_exists('synergywholesale_microsoft365' . '_' . $method));
+    }
+}
